df13d34299c0081a028c9e0a63996d261c73254f
tsemach <tsemach.mizrachi>
2018-04-07 13:58:39 +0300

    Add library

---
+++ database/config/db/config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "loopback",
+    "host": "172.17.0.2",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "loopback",
+    "host": "172.17.0.2",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "loopback",
+    "host": "172.17.0.2",
+    "dialect": "postgres"
+  }
+};
--- database/src/tutorial/database.js
+++ database/src/tutorial/database.js
@@ -1,20 +1,5 @@
-** Example of using sequelize with postgress and mongodb **
 
-## Database Commands
-* docker run -p 5432:5432 --name db -e POSTGRES_PASSWORD=postgres -d postgres:9.6.6-alpine
-
-## Running migration tool
-** this will create the directories ./models ./migrations and ./seeders **
-** also files defining the data types will also generated.
-
-node_modules/.bin/sequelize init
-node_modules/.bin/sequelize  model:generate --name Session --attributes user:string,task:string,task_md5:uuid,event:string,event_md5:uuid
-node_modules/.bin/sequelize db:migrate
-node_modules/.bin/sequelize db:seed:all
-
-```javascript
 const Sequelize = require('sequelize');
-const Session = sequelize.import('../../models/session');
 
 // connect to the database
 const sequelize = new Sequelize('loopback', 'postgres', 'postgres', {
@@ -22,6 +7,7 @@ const sequelize = new Sequelize('loopback', 'postgres', 'postgres', {
 });
 
 const User = sequelize.import('../../models/user');
+const Session = sequelize.import('../../models/session');
 
 let user = User.build({name: "tsemach", age: 56})
 let session = Session.build({name: "tsemach", task: "{}", task_md5: "705053ace1ec44e3c6bf5a72d7e5d644", event: "{}", event_md5: "705053ace1ec44e3c6bf5a72d7e5d644"})
@@ -33,5 +19,6 @@ user.save()
 session.save()
     .then(() => console.log("session wrote to db ok"))
     .catch(what => console.log("ooops .. something bad happend - " + what));
-```
+
+
 
